0df9e6df28791cc9c508193fa786811ab4bd78cc
Zouhair <zouhairsahtout66@gmail.com>
2022-03-24 12:16:40 -0400

    I added two buttons

--- index.php
+++ index.php
@@ -3,5 +3,7 @@
 
 <?php include('includes/header.php'); ?>
     <button type="button" class="btn btn-primary">Click me</button>
+    <button type="button" class="btn btn-success">Click me</button>
+    <button type="button" class="btn btn-danger">Click me</button>
 
 <?php require 'includes/footer.php'; ?>
