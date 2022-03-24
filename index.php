6ea5dfb1a3d56cbc5ce4bcd2c6b8b60071a5abe4
Zouhair <zouhairsahtout66@gmail.com>
2022-03-24 14:11:41 -0400

    Added link to Heroku

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
     <button type="button" class="btn btn-primary">Click me</button>
     <button type="button" class="btn btn-success">Click me</button>
     <button type="button" class="btn btn-danger">Click me</button>
+    <a href="https://www.heroku.com" class="btn btn-warning" target="_blank">Heroku</a>
 
 <?php require 'includes/footer.php'; ?>
